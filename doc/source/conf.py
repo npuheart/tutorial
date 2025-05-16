e06d84b1b80ce9f89bfcc2ddd67a88ff6fc62b5a
shaoyaoqian <mpf_npu@icloud.com>
2025-05-16 22:05:41 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -86,7 +86,7 @@ source_suffix = {
 
 
 bibtex_bibfiles = ['references.bib']
-bibtex_reference_style = 'number'
+bibtex_reference_style = "author_year"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates", get_template_dir()]
